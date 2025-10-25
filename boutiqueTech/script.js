cd52204e228c1d1f862ef519c52d8451bf0dd578
TheBread052 <matteobronner@gmail.com>
2025-10-25 19:06:52 +0200

    modification nom images

--- boutiqueTech/script.js
+++ boutiqueTech/script.js
@@ -4,6 +4,18 @@ fetch('/boutiqueTech/data.json')
         const zoneProduit = document.getElementById('produits');
         zoneProduit.className = "zoneProduit";
 
+        const zoneAres = document.createElement('div');
+        zoneAres.className = "zoneAres"
+
+        const zoneBucky = document.createElement('div');
+        zoneBucky.className = "zoneBucky"
+
+        const zoneBulldog = document.createElement('div');
+        zoneBulldog.className = "zoneBucky"
+
+        const zoneClassic = document.createElement('div');
+        zoneClassic.className = "zoneClassic"
+
         data.Ares.forEach(produit => {
             const produitCarte = document.createElement('div');
             produitCarte.className = "carteProduit"
@@ -31,10 +43,107 @@ fetch('/boutiqueTech/data.json')
 
             produitCarte.appendChild(produitTexte);
             produitCarte.appendChild(image);
-            
-            zoneProduit.appendChild(produitCarte);
 
+            zoneAres.appendChild(produitCarte);
         });
+
+        data.Bucky.forEach(produit => {
+            const produitCarte = document.createElement('div');
+            produitCarte.className = "carteProduit"
+
+            const produitTexte = document.createElement('div');
+            produitTexte.className = "texteProduit"
+            
+            const name = document.createElement('p');
+            name.textContent = `Nom: ${produit.nom}`;
+
+            const price = document.createElement('p');
+            price.textContent = `Prix: ${produit.prix}`;
+
+            const desc = document.createElement('p');
+            desc.textContent = `Description: ${produit.description}`
+
+            const image = document.createElement('img');
+            image.src = produit.Image
+            image.className = "imageProduit";
+
+
+            produitTexte.appendChild(name);
+            produitTexte.appendChild(price);
+            produitTexte.appendChild(desc);
+
+            produitCarte.appendChild(produitTexte);
+            produitCarte.appendChild(image);
+
+            zoneBucky.appendChild(produitCarte);
+        });
+
+        data.Bulldog.forEach(produit => {
+            const produitCarte = document.createElement('div');
+            produitCarte.className = "carteProduit"
+
+            const produitTexte = document.createElement('div');
+            produitTexte.className = "texteProduit"
+            
+            const name = document.createElement('p');
+            name.textContent = `Nom: ${produit.nom}`;
+
+            const price = document.createElement('p');
+            price.textContent = `Prix: ${produit.prix}`;
+
+            const desc = document.createElement('p');
+            desc.textContent = `Description: ${produit.description}`
+
+            const image = document.createElement('img');
+            image.src = produit.Image
+            image.className = "imageProduit";
+
+
+            produitTexte.appendChild(name);
+            produitTexte.appendChild(price);
+            produitTexte.appendChild(desc);
+
+            produitCarte.appendChild(produitTexte);
+            produitCarte.appendChild(image);
+
+            zoneBulldog.appendChild(produitCarte);
+        });
+
+        data.Classic.forEach(produit => {
+            const produitCarte = document.createElement('div');
+            produitCarte.className = "carteProduit"
+
+            const produitTexte = document.createElement('div');
+            produitTexte.className = "texteProduit"
+            
+            const name = document.createElement('p');
+            name.textContent = `Nom: ${produit.nom}`;
+
+            const price = document.createElement('p');
+            price.textContent = `Prix: ${produit.prix}`;
+
+            const desc = document.createElement('p');
+            desc.textContent = `Description: ${produit.description}`
+
+            const image = document.createElement('img');
+            image.src = produit.Image
+            image.className = "imageProduit";
+
+
+            produitTexte.appendChild(name);
+            produitTexte.appendChild(price);
+            produitTexte.appendChild(desc);
+
+            produitCarte.appendChild(produitTexte);
+            produitCarte.appendChild(image);
+
+            zoneClassic.appendChild(produitCarte);
+        });
+
+        zoneProduit.appendChild(zoneAres);
+        zoneProduit.appendChild(zoneBucky);
+        zoneProduit.appendChild(zoneBulldog);
+        zoneProduit.appendChild(zoneClassic)
     });
 
     
